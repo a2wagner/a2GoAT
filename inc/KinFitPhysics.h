d12921f9c9ed26d61055ab02c5aaac77751d1588
Sascha Wagner <wagners@kph.uni-mainz.de>
2015-02-13 00:08:53 +0100

    Added a generic struct particle_t and definitions to store essential particle information and check for final states; at least it compiled

--- inc/KinFitPhysics.h
+++ inc/KinFitPhysics.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <cstdio>
 #include <string>
+#include <algorithm>
 
 #include "GTreeManager.h"
 #include "GTreePluto.h"
@@ -12,12 +13,107 @@
 #include "KinFit.h"
 #include "TRandom3.h"
 
+/* prepare a special enum class where one can check if a value was defined in the enum */
+template <typename T>
+struct enum_traits {
+	static constexpr bool is_enum = std::is_enum<T>::value;
+	static constexpr bool sorted = false;
+};
+
+template<typename T, size_t N>
+T *endof(T (&ra)[N]) {
+	return ra + N;
+}
+
+template<typename T, typename ValType>
+T check(ValType v) {
+	typedef enum_traits<T> traits;
+	const T *first = traits::enumerators;
+	const T *last = endof(traits::enumerators);
+	if (traits::sorted) {  // probably premature optimization
+		if (std::binary_search(first, last, v))
+			return T(v);
+	} else if (std::find(first, last, v) != last) {
+		return T(v);
+	}
+	throw false;
+}
+
+// "enhanced" definition of enum
+enum particle_id {
+	rootino = 0,
+	photon = 1,
+	positron,
+	electron,
+	antimuon = 5,
+	muon,
+	piplus = 8,
+	piminus,
+	neutron = 13,
+	proton = 14
+};
+
+/*template<>
+struct enum_traits<particle_id> {
+	static constexpr bool is_enum = std::is_enum<particle_id>::value;
+	static constexpr bool sorted = true;
+	static const particle_id enumerators[];
+};
+
+constexpr particle_id enum_traits<particle_id>::enumerators[] = {
+	rootino,
+	photon,
+	positron,
+	electron,
+	antimuon,
+	muon,
+	piplus,
+	piminus,
+	proton
+};*/
+
+
+template<>
+struct enum_traits<particle_id> {
+	static constexpr bool is_enum = std::is_enum<particle_id>::value;
+	static constexpr bool sorted = true;
+	static constexpr particle_id enumerators[] = {
+		rootino,
+		photon,
+		positron,
+		electron,
+		antimuon,
+		muon,
+		piplus,
+		piminus,
+		neutron,
+		proton
+	};
+};
+
+//typedef std::pair<TLorentzVector, particle_id> particle_t;
+struct particle_t {
+	TLorentzVector p4;
+	particle_id id;
+	particle_t() : p4(), id(rootino) {}
+	particle_t(TLorentzVector p4, particle_id id) : p4(p4), id(id) {}
+	bool is_final_state()
+	{
+		return check<particle_id>(id);
+	}
+};
+
 class KinFitPhysics : public PPhysics
 {
 private:
-    typedef std::vector<std::vector<TLorentzVector>> vvP4;
-    typedef vvP4::iterator outer_it;
-    typedef std::vector<TLorentzVector>::iterator inner_it;
+
+	typedef std::vector<TLorentzVector> p4vector;
+	typedef std::vector<p4vector> vvP4;
+	typedef vvP4::iterator outer_it;
+	typedef p4vector::iterator inner_it;
+
+	typedef std::vector<particle_t> particle_vector;
+	typedef particle_vector::iterator particle_it;
 
 	GH1* time;
 	GH1* time_cut;
